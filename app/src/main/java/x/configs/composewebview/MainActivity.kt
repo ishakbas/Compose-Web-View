ce3d6495341f18d8ca2f87e0f2429d691e528d80
Michael <brawlcygan@gmail.com>
2023-03-27 00:37:46 +0300

    fix

--- app/src/main/java/x/configs/composewebview/MainActivity.kt
+++ app/src/main/java/x/configs/composewebview/MainActivity.kt
@@ -2,63 +2,68 @@ package x.configs.composewebview
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.pm.PackageManager
-import android.os.Build
+import android.content.Context
 import android.os.Bundle
 import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import x.configs.composewebview.ui.theme.ComposeWebViewTheme
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        requestPermissions(arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
         setContent {
             ComposeWebViewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    WebViewShow()
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MyWebView(context = this)
                 }
             }
         }
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(
-                arrayOf(
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.MANAGE_EXTERNAL_STORAGE,
-                    Manifest.permission.RECORD_AUDIO), 5)
-        }
+
     }
 }
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun WebViewShow() {
+fun MyWebView(context: Context) {
 
-//    var url = "https://puzoki.github.io/vue-portfolio/"
-    var url = "https://puzoki.github.io/demo-project/"
+    val url = "https://puzoki.github.io/demo-project/"
 
+    val myWebView = WebViewClient()
+    val myWebViewChr = object : WebChromeClient() {
+
+        override fun onPermissionRequest(request: PermissionRequest?) {
+            Toast.makeText(context, "Сайт хочет камеры", Toast.LENGTH_LONG).show()
+            request?.grant(request.resources)
+        }
+
+    }
     Column(modifier = Modifier.fillMaxSize()) {
+
         AndroidView(factory = {
+
             WebView(it).apply {
+
                 layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
-                webViewClient = WebViewClient()
+                webViewClient = myWebView
+                webChromeClient = myWebViewChr
+
                 settings.javaScriptEnabled = true
                 settings.allowContentAccess = true
                 settings.allowFileAccess = true
@@ -70,6 +75,5 @@ fun WebViewShow() {
         },
             update = { it.loadUrl(url) }
         )
-
     }
 }
